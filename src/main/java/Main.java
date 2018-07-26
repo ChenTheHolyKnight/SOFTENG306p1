c13cbad93425ee323bb66eeb661eadd7d58e0591
Darcy Cox <darcycox97@gmail.com>
2018-07-26 22:21:56 +1200

    Grade build set up, incl. dependencies

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("It works!");
+    }
+
+    public static int returnTwo() {
+        return 2;
+    }
+}
