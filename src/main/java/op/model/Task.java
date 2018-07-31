7b2cbe1891a7c077d217e6750e9df2eb9d2b9cba
Sam Broadhead <31305522+swimuel@users.noreply.github.com>
2018-07-31 17:08:02 +1200

    Revert "Op 24"

--- src/main/java/op/model/Task.java
+++ src/main/java/op/model/Task.java
@@ -1,15 +1,14 @@
-
 package op.model;
 
 /**
- *
+ * 
  * @author Ravid
  * Class used to represent Node of a graph for graph visualization.
  */
 public class Task {
 	private int id;
 	private int weight;
-
+	
 	public Task (int id, int weight) {
 		this.id = id;
 		this.weight = weight;
@@ -22,7 +21,7 @@ public class Task {
 	public int getWeight() {
 		return weight;
 	}
-
+	
 	@Override
 	public boolean equals(Object task) {
 		return ((Task) task).getId() == id;
