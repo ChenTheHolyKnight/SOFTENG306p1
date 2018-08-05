e04e5876832c8081538b4f6b1b09691a0cab41af
Darcy Cox <darcycox97@gmail.com>
2018-08-05 12:19:01 +1200

    OP-66 Structure of DotIn tests figured out

--- src/test/java/TestDotIO.java
+++ src/test/java/TestDotIO.java
@@ -1,19 +1,44 @@
 import op.algorithm.SimpleScheduler;
 import op.io.DotIO;
+import op.model.Task;
 import op.model.TaskGraph;
 import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
 import java.io.IOException;
-import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+/**
+ * Test class checking that the dot file IO module correctly initializes the TaskGraph singleton.
+ * @author Darcy Cox
+ */
 public class TestDotIO {
 
+    // constants to hold the paths to each dot file
+    private static final String BASE_DIR = "src/main/resources/sample_inputs/";
+    private static final String SAMPLE_7 = BASE_DIR + "Nodes_7_OutTree.dot";
+    private static final String SAMPLE_8 = BASE_DIR + "Nodes_8_Random.dot";
+    private static final String SAMPLE_9 = BASE_DIR + "Nodes_9_SeriesParallel.dot";
+    private static final String SAMPLE_10 = BASE_DIR + "Nodes_10_Random.dot";
+    private static final String BASIC = BASE_DIR + "test.dot";
 
+    private static DotIO dotIO;
+
+    @BeforeClass
+    public static void initIO() {
+        dotIO = new DotIO();
+    }
+
+    // task graph can only be initialized once so we must reset it after each test
     @After
-    public void resetSingleton() {
+    public void resetTaskGraph() {
         try {
             SingletonTesting.resetTaskGraph();
         } catch (NoSuchFieldException e) {
@@ -24,28 +49,54 @@ public class TestDotIO {
     }
 
     @Test
-    public void testAddTask(){
-        DotIO testIo = new DotIO();
-        try {
-            testIo.dotIn("src/main/resources/sample_inputs/test.dot");
+    public void testBasic() throws IOException {
+        List<Task> tasksExpected = new ArrayList<Task>();
+        Collections.addAll(
+                tasksExpected,
+                new Task("1", 2),
+                new Task("2", 3),
+                new Task("3", 3),
+                new Task("4", 2)
+        );
 
-            // this is commented because we don't want the repo/travis to write files. need to figure out
-            // a good way of testing File output operations. Feel free to uncomment and check the output file but
-            // make sure it's not committed to the repo.
-            //testIo.dotOut("out.dot", new SimpleScheduler().produceSchedule(1));
-        } catch (IOException e) {
-            e.printStackTrace();
-            fail();
-        }
+        dotIO.dotIn(BASIC);
+        List<Task> tasksActual = TaskGraph.getInstance().getAllTasks();
+        checkTaskListsAreEquivalent(tasksExpected, tasksActual);
     }
+
 
     @Test
-    public void testAddDependency(){
-        DotIO testio = new DotIO();
-        try {
-            testio.dotIn("src/main/resources/sample_inputs/test.dot");
-        } catch (IOException e) {
-            fail();
-        }
+    public void testSample7() throws IOException{
+
+        //TODO: check title is the same, tasks, and dependencies
+        // expected
+        List<Task> tasksExpected = new ArrayList<Task>();
+        Collections.addAll(
+                tasksExpected,
+                new Task("0", 5),
+                new Task("1", 6),
+                new Task("2", 5),
+                new Task("3", 6),
+                new Task("4", 4),
+                new Task("5", 7),
+                new Task("6", 7)
+        );
+
+        dotIO.dotIn(SAMPLE_7);
+        TaskGraph tg = TaskGraph.getInstance();
+        List<Task> tasksActual = tg.getAllTasks();
+
+        assertEquals(tasksActual.size(), tasksExpected.size());
+
     }
+
+
+    private static void checkTaskListsAreEquivalent(List<Task> expected, List<Task> actual) {
+        // TODO ensure that weights are also the same for each task
+        assertEquals(expected.size(), actual.size());
+        assertTrue(expected.containsAll(actual));
+        assertTrue(actual.containsAll(expected));
+    }
+
+
 }
