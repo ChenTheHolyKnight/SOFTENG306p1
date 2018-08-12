7f769dd965a7eed0ca64c472778e1934c8bb352a
Chen <czha959@aucklanduni.ac.nz>
2018-08-12 20:37:02 +1200

    Fixed Merge conflict

--- src/main/java/op/visualization/controller/GUIController.java
+++ src/main/java/op/visualization/controller/GUIController.java
@@ -1,4 +1,56 @@
-package op.visualization.controller;
+package op.visualization.view;
 
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+import org.controlsfx.control.ToggleSwitch;
+
+/**
+ * The controller class to control the GUI*/
 public class GUIController {
+    @FXML
+    public ToggleSwitch graphSwitch;
+
+    @FXML
+    public AnchorPane graphPane;
+
+    @FXML
+    public AnchorPane schedulePane;
+
+
+
+
+    /**
+     * method to control the switch when the toggle switch is triggered
+     */
+    @FXML
+    public void onSwitchTriggered(){
+        boolean selected=this.graphSwitch.isSelected();
+        if(!selected){
+            FadeTransition fadeout = new FadeTransition(Duration.millis(500), schedulePane); //fade out schedulePane
+            fadeout.setFromValue(1.0);
+            fadeout.setToValue(0.0);
+            fadeout.setOnFinished(event -> {
+                fadeout.stop();
+            });
+            fadeout.playFromStart();
+        }else {
+            FadeTransition fadeout = new FadeTransition(Duration.millis(500), schedulePane); //fade in schedulePane
+            fadeout.setFromValue(0.0);
+            fadeout.setToValue(1.0);
+            fadeout.setOnFinished(event -> {
+                fadeout.stop();
+            });
+            fadeout.playFromStart();
+        }
+
+    }
+
+    @FXML
+    public void initialize(){
+        schedulePane.setOpacity(0.0);
+    }
+
+
 }
