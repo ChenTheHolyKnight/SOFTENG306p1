35599c280a1489c318c5b22578d4f06c2b3c8454
Chen <czha959@aucklanduni.ac.nz>
2018-08-13 13:07:38 +1200

    OP-12-Chen changes the implementation of tab pane

--- src/main/java/op/visualization/controller/GUIController.java
+++ src/main/java/op/visualization/controller/GUIController.java
@@ -26,6 +26,14 @@ public class GUIController {
     @FXML
     private AnchorPane schedulePane;
 
+
+    private Scene scene;
+
+    private boolean selected=false;
+
+
+
+
     /**
      * method to control the switch when the toggle switch is triggered
      */
@@ -52,28 +60,7 @@ public class GUIController {
 
     }
 
-    /**
-     * Called when GUIController is instantiated. Sets up GUI.
-     */
-    @FXML
-    public void onTabClicked(){
-        Translate translate=new Translate();
-        Tab tab=tabPane.getSelectionModel().getSelectedItem();
-        AnchorPane pane=(AnchorPane) tab.getContent();
-        if(!selected) {
-            selected=true;
-            this.scene.getWindow().setWidth(this.scene.getWindow().getWidth() + tabPane.getWidth());
 
-
-            translate.setX(pane.getWidth());
-
-        }else{
-            selected=false;
-            this.scene.getWindow().setWidth(this.scene.getWindow().getWidth() - tabPane.getWidth());
-            translate.setX(-pane.getWidth());
-        }
-        tabPane.getTransforms().add(translate);
-    }
 
     public void setScene(Scene scene) {
         this.scene = scene;
