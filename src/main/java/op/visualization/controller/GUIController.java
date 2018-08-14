5fdbfb23c46f5146eaca64bf014fc9a6719a1c12
Chen <czha959@aucklanduni.ac.nz>
2018-08-14 23:03:18 +1200

    OP-12-Chen successfully add one scheduled task to the graph

--- src/main/java/op/visualization/controller/GUIController.java
+++ src/main/java/op/visualization/controller/GUIController.java
@@ -8,11 +8,14 @@ import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
+import op.model.Schedule;
 import op.model.ScheduledTask;
+import op.model.Task;
 import op.model.TaskGraph;
 import op.visualization.GanttChart;
 import op.Application;
@@ -25,6 +28,7 @@ import org.graphstream.ui.view.GraphRenderer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -55,7 +59,7 @@ public class GUIController {
 
 
 
-
+    private HashMap<Integer,XYChart.Series> seriesHashMap=new HashMap<>();
 
 
     private Scene scene;
@@ -63,6 +67,8 @@ public class GUIController {
     private boolean selected=false;
 
     private int coreNum=5;
+
+
 
 
 
@@ -160,6 +166,7 @@ public class GUIController {
         List<String> processors=new ArrayList<>();
         for(int i=0;i<coreNum;i++){
             processors.add("Processor"+(i+1));
+            seriesHashMap.put(i,new XYChart.Series());
         }
 
         //set up Axis and Chart
@@ -178,10 +185,14 @@ public class GUIController {
         chart.setBlockHeight( 50);
         chart.setPrefHeight(schedulePane.getPrefHeight());
         chart.setPrefWidth(schedulePane.getPrefWidth());
+        chart.getStylesheets().add(GUIController.class.getResource("../view/Styles/ganttchart.css").toExternalForm());
 
-        
         //add chart to the pane
         schedulePane.getChildren().add(chart);
+
+        Task task=new Task("1",2);
+        ScheduledTask task1=new ScheduledTask(task,1,2);
+        addScheduledTaskToChart(task1);
 
 
     }
@@ -191,6 +202,11 @@ public class GUIController {
      */
     public void addScheduledTaskToChart(ScheduledTask task){
         int weight=task.getTask().getDuration();
+        int processorNum=task.getProcessor();
+        XYChart.Series series=seriesHashMap.get(processorNum-1);
+        series.getData().add(new XYChart.Data(0, yAxis.getCategories().get(processorNum-1),
+                new GanttChart.ExtraData( 1, "status-blue")));
+        chart.getData().add(series);
 
     }
 }
