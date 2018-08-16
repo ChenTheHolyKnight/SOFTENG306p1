117c481b5057ef05d77187a763c50f7df7424951
swimuel <sambroadhead@icloud.com>
2018-08-17 11:07:38 +1200

    OP-129 switch back to normal

--- src/main/java/op/algorithm/DFSParallelScheduler.java
+++ src/main/java/op/algorithm/DFSParallelScheduler.java
@@ -54,24 +54,24 @@ public class DFSParallelScheduler extends BranchAndBoundScheduler {
         // initiate threads and run them in parallel
         ExecutorService executor = Executors.newFixedThreadPool(numThreads);
         List<Future<Schedule>> futures = new ArrayList<>();
-        stacks = new Stack[numThreads];
+        /*stacks = new Stack[numThreads];
         for (int i = 0; i < numThreads; i++){
             stacks[i] = new Stack<>();
         }
         int scheduleSize = scheduleStack.size(); // for loop iteration safety
         for (int i = 0; i < scheduleSize; i++){
             stacks[i%numThreads].push(scheduleStack.pop());
-        }
+        }*/
         for (int i = 0; i < numThreads; i++) {
-            futures.add(executor.submit(new DFSScheduler(getNumProcessors(), getPrunerManager(), getCostFunctionManager().clone(), stacks[i])));
+            futures.add(executor.submit(new DFSScheduler(getNumProcessors(), getPrunerManager(), getCostFunctionManager().clone(), scheduleStack)));
         }
         executor.shutdown();
-        while(!executor.isTerminated()){
+        /*while(!executor.isTerminated()){
             for (int i = 0; i < numThreads; i++){
                 int neighbor = (i+1)%numThreads;
                 /*if((stacks[i].size()+10 < stacks[neighbor].size()) && (stacks[neighbor].size() > 4)){
                     stacks[i].push(stacks[neighbor].pop());
-                }*/
+                }
                 if(stacks[i].isEmpty() && !(stacks[neighbor].size() < 2)){
                     int splitSize = stacks[neighbor].size()/2;
                     for (int j = 0; j < splitSize; j++){
@@ -79,7 +79,7 @@ public class DFSParallelScheduler extends BranchAndBoundScheduler {
                     }
                 }
             }
-        }
+        }*/
 
         List<Schedule> results = new ArrayList<>();
         for (Future<Schedule> f : futures) {
