01e2f8d281fb551b427c94db7b207a1860a3abdf
Darcy Cox <darcycox97@gmail.com>
2018-08-19 12:34:57 +1200

    OP-158 Write DRT cost func test

--- src/test/java/TestCostFunction.java
+++ src/test/java/TestCostFunction.java
@@ -1,4 +1,5 @@
 import op.algorithm.bound.BottomLevelFunction;
+import op.algorithm.bound.DataReadyTimeFunction;
 import op.algorithm.bound.IdleTimeFunction;
 import op.io.DotIO;
 import op.model.Schedule;
@@ -103,6 +104,30 @@ public class TestCostFunction {
     public void testDataReadyTimeCostFunction() {
         initNodes7Graph();
 
+        List<Task> tasks = TaskGraph.getInstance().getAllTasks();
+
+        DataReadyTimeFunction drt = new DataReadyTimeFunction(2); // 2 processors
+
+        // test empty schedule
+        Schedule s = new Schedule();
+        // for this input graph, and the empty schedule, the free task will be task "0"
+        // its data ready time is 0 and its bottom level is 18
+        assertEquals(18, drt.calculate(s));
+
+        Task t = tasks.get(tasks.indexOf(new Task("0", 5)));
+        s.addScheduledTask(new ScheduledTask(t, 0, 1));
+        t = tasks.get(tasks.indexOf(new Task("1", 6)));
+        s.addScheduledTask(new ScheduledTask(t, 5, 1));
+        t = tasks.get(tasks.indexOf(new Task("2", 5)));
+        s.addScheduledTask(new ScheduledTask(t, 11, 1));
+
+        // test case with lots of free tasks. In this case, tasks 3, 4, 5, and 6 are free
+        // their DRTs + bottom levels are:
+        // 3: 16 + 6 = 22
+        // 4: 16 + 4 = 20
+        // 5: 15 + 7 = 22
+        // 6: 16 + 7 = 23 (tightest bound, should be the result)
+        assertEquals(23, drt.calculate(s));
     }
 
 
